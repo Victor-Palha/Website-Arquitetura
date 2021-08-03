338251a019253d6f69040adee517cff6fd1ea111
Victor-Palha <87082135+Victor-Palha@users.noreply.github.com>
2021-08-03 01:56:17 -0300

    Update e-validation.php

--- P.Felipe/e-validation.php
+++ P.Felipe/e-validation.php
@@ -1,5 +1,4 @@
 <?php
-
 date_default_timezone_set('America/Sao_Paulo');
 require_once('src/PHPMailer.php');
 require_once('src/SMTP.php');
@@ -10,55 +9,45 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 
+$nome = isset($_POST['nome']) ?  $_POST['nome'] : 'Não informado';
+$email = isset($_POST['email']) ?  $_POST['email'] : 'Não informado';
+$assunto = isset($_POST['assunto']) ?  $_POST['assunto'] : 'Não informado';
+$conteudo = isset($_POST['conteudo']) ?  $_POST['conteudo'] : 'Não informado';
+$data = date('d/m/Y H:i:s');
+
+if($email && $conteudo){
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'seuemail@gmail.com';
+    $mail->Password = 'suasenha';
+    $mail->Port = 587;
+
+    $mail->setFrom('seuemail@gmail.com');
+    $mail->addAddress('seuemaila@gmail.com');
+
+    $mail->isHTML(true);
+    $mail->Subject = $assunto;
+    $mail->Body = "Nome: {$nome} <br>
+                    E-mail: {$email} <br>
+                    Mensagem: {$conteudo} <br>
+                    Data/Hora: {$data}";
 
 
-if($_POST){
-    $nome = !empty($_POST['nome']) ?  $_POST['nome'] : 'Não informado';
-    $email = !empty($_POST['email']) ?  trim($_POST['email']) : 'Não informado';
-    $assunto = !empty($_POST['assunto']) ?  utf8_decode($_POST['assunto']) : 'Não informado';
-    $conteudo = !empty($_POST['conteudo']) ?  trim($_POST['conteudo']) : 'Não informado';
-    $data = date('d/m/Y H:i:s');
-    if($_POST['email'] && $_POST['conteudo']){
-        $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'lipepalha@gmail.com';
-        $mail->Password = 'fe141295';
-        $mail->Port = 587;
-
-        $mail->setFrom('lipepalha@gmail.com');
-        $mail->addAddress('lipepalha@gmail.com');
-
-        $mail->isHTML(true);
-        $mail->Subject = $assunto;
-        $mail->Body = "Nome: {$nome} <br>
-                        E-mail: {$email} <br>
-                        Mensagem: {$conteudo} <br>
-                        Data/Hora: {$data}";
-
-
-        if($mail->send()) {
-            echo "
-            <script> 
-            alert('Mensagem enviada com sucesso!')
-            </script>
-            ";
-        } else {
-            echo "
-            <script> 
-            alert('Erro! E-mail não enviado, tente novamente mais tarde!')
-            </script>
-                ";
-        }
-    }else{
+    if($mail->send()) {
         echo "
         <script> 
-        alert('Erro ao enviar mensagem, verifique as informações!')
+        alert('Mensagem enviada com sucesso!')
         </script>
         ";
+    } else {
+        echo "
+        <script> 
+        alert('Erro! E-mail não enviado, tente novamente mais tarde!')
+        </script>
+            ";
     }
-    
 }else{
         echo "
         <script> 
@@ -66,7 +55,6 @@ if($_POST){
         </script>
         ";
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -107,6 +95,5 @@ if($_POST){
     
 </main>
     <script src="header.js"></script>
-    <script src="slides.js"></script>
 </body>
 </html>
